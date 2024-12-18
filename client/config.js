e9449193cf14202bfd2c338eae23265c8feb7018
pavan prasad <pavanprasad0506@gmail.com>
2024-12-18 15:41:41 +0530

    names

--- client/config.js
+++ client/config.js
@@ -1,4 +1,5 @@
 export const Config = {
-    BACKEND_URL: 'http://23.20.220.126:3000',
-    //BACKEND_URL: 'http://localhost:3000',
+    //BACKEND_URL: 'http://23.20.220.126:3000',
+    BACKEND_URL: 'http://localhost:3000',
 };
+//hi iam pavan
